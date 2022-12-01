21e33e2ada011a603ab56efee035b2f10294d83e
Łukasz Wojewnik <l.wojewnik@wp.pl>
2022-12-01 14:26:36 +0100

    TODO

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+=====================================================================
+			TODO:
+	1. Utworzenie Tabeli 
+	2. Dodawanie nowych elementów
+	3. Usuwanie istniejących elementów
+	4. Edycja istniejących elementów
+	5. Przesuwanie elementów
+	6. Dodanie formularza
+	7. Dodanie komunikatów po każdej z czynności
+    8. Utworzenie LocalStorage
+    9. Pobieranie danych z LocalStorage i ich wyświetlenie 
+	
+=====================================================================
+*/
