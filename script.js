b9b7553029ab93b867307c1e6e945c75e468268b
Łukasz Wojewnik <l.wojewnik@wp.pl>
2022-12-01 19:11:51 +0100

    Add form + button to create new elements in table

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 	3. Usuwanie istniejących elementów ----------------------  Done
 	4. Edycja istniejących elementów    ---------------------  Done
 	5. Przesuwanie elementów    -----------------------------
-	6. Dodanie formularza   ---------------------------------
+	6. Dodanie formularza   ---------------------------------  Done
 	7. Dodanie komunikatów po każdej z czynności    ---------
     8. Utworzenie LocalStorage  -----------------------------
     9. Pobieranie danych z LocalStorage i ich wyświetlenie --
@@ -18,6 +18,7 @@
 =====================================================================
 */
 var table = document.createElement("table");
+var form;
 initializeTable();
 //addNewElementToTable("Gruszka",1.5,3);    //Test dla dodawania elementu
 //addNewElementToTable("Banan",2,8);        //Test dla dodawania elementu
@@ -30,8 +31,18 @@ function initializeTable() {
     addElementToRow(row,"Cena");
     addElementToRow(row,"Ilość");
     addElementToRow(row,"Suma");
-    addElementToRow(row,'\xa0');
-    addElementToRow(row,'\xa0');
+    let cell = document.createElement("td");
+    let button = document.createElement("input");
+    button.setAttribute("type","button");
+    button.value = "Nowy produkt";
+    cell.colSpan = 2;
+    button.addEventListener("click",() => {
+        if(form == null) {
+            createForm("Dodaj");
+        }
+    })
+    cell.appendChild(button);
+    row.appendChild(cell);
     table.appendChild(row);
     document.body.appendChild(table);
 }
@@ -74,4 +85,69 @@ function editElementFromTalbe(id,name,price,quantity) {
     activeRow.children[2].textContent = price;
     activeRow.children[3].textContent = quantity;
     activeRow.children[4].textContent = price*quantity;
+}
+function createForm(buttonText) {
+    form = document.createElement("form");
+    form.setAttribute("method","post");
+    let br = document.createElement("br");
+
+    let nameLabel = document.createElement("label");
+    let nameInput = document.createElement("input");
+    nameLabel.textContent = "Nazwa";
+
+    let priceLabel = document.createElement("label");
+    let priceInput = document.createElement("input");
+    priceInput.setAttribute("type","number");
+    priceLabel.textContent = "Cena";
+
+    let quantityLabel = document.createElement("label");
+    let quantityInput = document.createElement("input");
+    quantityLabel.textContent = "Ilość";
+    let applyButton = document.createElement("input");
+    applyButton.setAttribute("type","button");
+    if(typeof buttonText === 'string') {
+        applyButton.value = buttonText;
+    } 
+    else {
+        applyButton.value = "Dodaj";
+    }
+    applyButton.addEventListener("click",() => {
+        console.log(nameInput.value);
+        console.log(priceInput.value);
+        console.log(quantityInput.value);
+        if(nameInput.value.length === 0) {
+            return;
+        }
+        if(priceInput.value.length === 0) {
+            return;
+        }
+        if(quantityInput.value.length === 0) {
+            return;
+        }
+        if(isNaN(parseFloat(priceInput.value)) || isNaN(parseInt(quantityInput.value))) {
+            return;
+        }
+        addNewElementToTable(nameInput.value,parseFloat(priceInput.value),parseInt(quantityInput.value));
+        form.remove();
+        form = null;
+    })
+    form.appendChild(nameLabel);
+    form.appendChild(br);
+    br = document.createElement("br");
+    form.appendChild(nameInput);
+    form.appendChild(br);
+    br = document.createElement("br");
+    form.appendChild(priceLabel);
+    form.appendChild(br);
+    br = document.createElement("br");
+    form.appendChild(priceInput);
+    form.appendChild(br);
+    br = document.createElement("br");
+    form.appendChild(quantityLabel);
+    form.appendChild(br);
+    br = document.createElement("br");
+    form.appendChild(quantityInput);
+    form.appendChild(br);
+    form.appendChild(applyButton);
+    document.body.appendChild(form);
 }
